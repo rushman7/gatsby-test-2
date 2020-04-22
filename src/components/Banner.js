d127cead32be93f59d89f2adc2ea9416cef00a62
BuildTools <arthurpisakhov1@gmail.com>
2020-04-22 17:10:52 -0400

    hello

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,10 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Hi, my name is Arthur</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>Web Developer</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
